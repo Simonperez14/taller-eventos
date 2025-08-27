16a20cd486c692157bd631a9ee30552d062c126e
Simon Perez <simonpc10911@gmail.com>
2025-08-27 02:06:05 -0300

    Solucion al doble clickeo

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById("div").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
+document.getElementById("boton").addEventListener("click", function(e) {
+    e.stopPropagation();
+    alert("Hola!");
+});
